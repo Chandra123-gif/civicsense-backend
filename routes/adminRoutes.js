bb053894f1387c4d9e83025787d7138179e16bfa
Chandra123-gif <chandrasekharuppu004@gmail.com>
2025-12-26 13:48:20 +0530

    Add missing export statement in adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Issue from "../models/Issue.js";
 import protect from "../middleware/authMiddleware.js";
-import adminOnly from "../middleware/adminMiddleware.js";
+import adminMiddleware from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
@@ -28,3 +28,4 @@ router.put("/issues/:id", protect, adminOnly, async (req, res) => {
 });
 
 export default router;   // ✅ THIS LINE WAS MISSING
+
