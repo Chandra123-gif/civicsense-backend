ccb40def40fba0e33abb9568dc8bf9351233ac15
Chandra123-gif <chandrasekharuppu004@gmail.com>
2025-12-29 23:08:46 +0530

    Update issueRoutes.js

--- routes/issueRoutes.js
+++ routes/issueRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { createIssue, myIssues } from "../controllers/issueController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 import upload from "../middleware/uploadMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, upload.single("image"), createIssue);
-router.get("/my", authMiddleware, myIssues);
+router.post(
+  "/",
+  authMiddleware,
+  upload.single("image"),
+  createIssue
+);
 
 export default router;
-
-
-
